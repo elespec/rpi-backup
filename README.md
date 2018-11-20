3f87934fd5deb0f52b5daf2efdd113177d92b1b6
guo.shipeng <guo.shipeng@xcar.com.cn>
2018-11-20 17:48:27 +0800

    sync

---
+++ README.md
@@ -0,0 +1,37 @@
+# rpi-backup
+本项目,自用脚本,持续使用中,基于下面的项目小小修改,提升了备份速度,修改了挂载方式;
+基于 https://github.com/conanwhf/RaspberryPi-script 修改
+
+
+# RaspberryPi-script
+树莓派相关配置&脚本，适用于rpi2B及3B硬件。
+
+## rpi-backup
+备份系统成一个可烧录的image，详细说明参见：http://conanwhf.github.io/2016/08/25/rpi-cloneimg/
+
+
+## rpi-clone
+一个超好用的备份工具，感谢开发者！
+来源：https://github.com/billw2/rpi-clone
+
+## build-编译脚本
+- build-toolchain.sh，在debian的系统中编译交叉编译工具的脚本
+- crossng_config，交叉编译工具编译配置文件，适用于rpi2B和rpi3B
+- kernel_config，内核配置，来源于官方
+- kernel.sh，内核编译脚本，跑在树莓派上
+- linux-mk.sh，内核编译脚本，跑在PC上，交叉编译
+
+## setup-配置脚本
+参考初始化配置运行顺序：network.sh-> init.sh-> dev-mod.sh
+- dev-mod.sh，一些开发用模块的装载和配置脚本，包括语音输出，开发环境，Airmon工具，Swift环境等
+- init.sh，初始配置，包括系统更新，vi & SSH配置，中文支持，USB供电修改，Samba支持等
+- interfaces,tightvncserver,vimrc，脚本运行所需对应的配置文件
+
+## sensor
+一些外部传感器获取状态的Demo，Python写成，rpi2B测试通过
+- libSetup.sh，开发包环境配置脚本，包括I2C,GPIO,UART,Bluetooth等
+- air_qu.py，空气质量传感器，Uart协议
+- bmp180.py，BMP180气压传感器，I2C
+- dht11.py，温度湿度传感器，GPIO
+- pcf8591.py，数模转换+照度传感，I2C
+- sample.py，各种协议操作的demo
---
+++ rpi-backup.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+
+#install tools
+sudo apt-get -y install rsync dosfstools parted kpartx exfat-fuse
+
+#mount USB device
+usbmount=/mnt
+mkdir -p $usbmount
+if [ -z $1 ]; then
+	echo "no argument, assume the mount device is /dev/sda1 ? Y/N"
+	read key
+	if [ "$key" = "y" -o "$key" = "Y" ]; then
+		sudo mount -o uid=1000 /dev/sda1 $usbmount
+	else
+		echo "$0 [backup dest device name], e.g. $0 /dev/sda1"
+		exit 0
+	fi
+else
+	sudo mount -t ntfs-3g -o big_writes $1 $usbmount
+fi
+if [ -z "`grep $usbmount /etc/mtab`" ]; then
+	echo "mount fail, exit now"
+	exit 0
+fi 
+
+img=$usbmount/rpi-`date +%Y%m%d-%H%M`-4k.img
+#img=$usbmount/rpi.img
+
+
+echo ===================== part 1, create a new blank img ===============================
+# New img file
+#sudo rm $img
+bootsz=`df -P | grep /boot | awk '{print $2}'`
+rootsz=`df -P | grep /dev/root | awk '{print $3}'`
+totalsz=`echo $bootsz $rootsz | awk '{print int(($1+$2)*1.3/4)}'`
+sudo dd if=/dev/zero of=$img bs=4K count=$totalsz
+
+# format virtual disk
+bootstart=`sudo fdisk -l /dev/mmcblk0 | grep mmcblk0p1 | awk '{print $2}'`
+bootend=`sudo fdisk -l /dev/mmcblk0 | grep mmcblk0p1 | awk '{print $3}'`
+rootstart=`sudo fdisk -l /dev/mmcblk0 | grep mmcblk0p2 | awk '{print $2}'`
+echo "boot: $bootstart >>> $bootend, root: $rootstart >>> end"
+#rootend=`sudo fdisk -l /dev/mmcblk0 | grep mmcblk0p2 | awk '{print $3}'`
+sudo parted $img --script -- mklabel msdos
+sudo parted $img --script -- mkpart primary fat32 ${bootstart}s ${bootend}s
+sudo parted $img --script -- mkpart primary ext4 ${rootstart}s -1
+loopdevice=`sudo losetup -f --show $img`
+device=/dev/mapper/`sudo kpartx -va $loopdevice | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+sleep 5
+sudo mkfs.vfat ${device}p1 -n boot
+sudo mkfs.ext4 ${device}p2
+
+
+echo ===================== part 2, fill the data to img =========================
+# mount partitions
+mountb=$usbmount/backup_boot/
+mountr=$usbmount/backup_root/
+mkdir -p $mountb $mountr
+# backup /boot
+sudo mount -t vfat ${device}p1 $mountb
+sudo cp -rfp /boot/* $mountb
+sync
+echo "...Boot partition done"
+# backup /root
+sudo mount -t ext4 ${device}p2 $mountr
+if [ -f /etc/dphys-swapfile ]; then
+        SWAPFILE=`cat /etc/dphys-swapfile | grep ^CONF_SWAPFILE | cut -f 2 -d=`
+	if [ "$SWAPFILE" = "" ]; then
+		SWAPFILE=/var/swap
+	fi
+	EXCLUDE_SWAPFILE="--exclude $SWAPFILE"
+fi
+sudo rsync --force -rltWDEgop --delete --stats --progress \
+	$EXCLUDE_SWAPFILE \
+	--exclude '.gvfs' \
+	--exclude '/dev' \
+    --exclude '/media' \
+	--exclude '/mnt' \
+	--exclude '/proc' \
+    --exclude '/run' \
+	--exclude '/sys' \
+	--exclude '/tmp' \
+    --exclude 'lost\+found' \
+	--exclude '$usbmount' \
+    --exclude '/data'\
+	--exclude '/export'\
+	// $mountr
+# special dirs 
+for i in dev media mnt proc run sys boot; do
+	if [ ! -d $mountr/$i ]; then
+		sudo mkdir $mountr/$i
+	fi
+done
+if [ ! -d $mountr/tmp ]; then
+	sudo mkdir $mountr/tmp
+	sudo chmod a+w $mountr/tmp
+fi
+sudo rm -f $mountr/etc/udev/rules.d/70-persistent-net.rules
+
+sync 
+ls -lia $mountr/home/pi/
+echo "...Root partition done"
+# if using the dump/restore 
+# tmp=$usbmount/root.ext4
+# sudo chattr +d $img $mountb $mountr $tmp
+# sudo mount -t ext4 ${device}p2 $mountr
+# cd $mountr
+# sudo dump -0uaf - / | sudo restore -rf -
+# cd
+
+
+# replace PARTUUID
+opartuuidb=`blkid -o export /dev/mmcblk0p1 | grep PARTUUID`
+opartuuidr=`blkid -o export /dev/mmcblk0p2 | grep PARTUUID`
+npartuuidb=`blkid -o export ${device}p1 | grep PARTUUID`
+npartuuidr=`blkid -o export ${device}p2 | grep PARTUUID`
+sudo sed -i "s/$opartuuidr/$npartuuidr/g" $mountb/cmdline.txt
+sudo sed -i "s/$opartuuidb/$npartuuidb/g" $mountr/etc/fstab
+sudo sed -i "s/$opartuuidr/$npartuuidr/g" $mountr/etc/fstab
+
+sudo umount $mountb
+sudo umount $mountr
+
+# umount loop device
+sudo kpartx -d $loopdevice
+sudo losetup -d $loopdevice
+rm -rf $mountb $mountr
+sudo umount $usbmount
+echo "==== All done. You can un-plug the backup device"
+
